bf7507e304a571c665b9628f72330b6ed29fcf23
Danish Mohammed <>
2021-04-24 11:09:12 +0000

    changed

--- student-habit-helper.py
+++ student-habit-helper.py
@@ -1,6 +1,10 @@
-<<<<<<< HEAD
 import time
 import random
+
+mood = 7
+productivity = 7
+energy = 7
+fitness = 7
 
 def intro():
     time.sleep(1)
@@ -34,49 +38,10 @@ while new_day[0] == "y":
     
 
 
-    
+    print("Mood: " + str(mood) + "/10")
     new_day = input("Would you like to start a another day? (yes/no)")
 
 print("Thank you for playing!") 
 print("Again, we want to remind you that some of these factors that we mentioned can simply just be out of your control.")
 print("But there are always things we can improve on, and the key here is constantly improving ourselves!")
 print("Hopefully you learned a few healthy habits that you want to implement for yourself. Now, back to the real world! Bye bye!")
-=======
-import time
-import random
-
-def intro():
-    time.sleep(1)
-    print("\n\t\t\t----------------------------------------------------------------------------------------")
-    print("\t\t\t|         Welcome to simulating a typical day in a high school students life!          |")
-    print("\t\t\t----------------------------------------------------------------------------------------")
-    time.sleep(4)
-    print("- This program is meant to give you an insight on some of the important decisions you make, even as a student studying from home!")
-    time.sleep(4)
-    print("- Habits are one of the most important aspects of human beings, but the pandemic has")
-    time.sleep(4)
-    print("- Try to answer honestly on the first try, you will get to simulate another day to try and improve your habits!")
-    time.sleep(4)
-
-def wake_up():
-    pass
-
-
-
-
-
-intro()
-
-new_day = "yes"
-while new_day == "yes" or new_day == "y":
-    
-
-
-    
-    new_day = input("Would you like to start a another day? (yes/no)")
-
-print("Thank you for playing!") 
-print("Again, we want to remind you that some of these factors that we mentioned can simply just be out of your control.")
-print("But there are always things we can improve on, and the key here is constantly improving ourselves!")
-print("Hopefully you learned a few healthy habits that you want to implement for yourself. Now, back to the real world! Bye bye!")
->>>>>>> origin/main
